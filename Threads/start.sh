e97219ab8f284857b21f7427380d281cec9e9f9b
Feruzbek-Sapayev <feruzbekcoder@gmail.com>
2025-06-25 12:13:19 +0500

    added gunicorn start.sh

---
+++ Threads/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Collect static files (optional)
+# python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Run the application
+gunicorn Threads.wsgi:application --bind 0.0.0.0:$PORT
